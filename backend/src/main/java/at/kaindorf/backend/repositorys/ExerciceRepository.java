52865c15a799827c1a58d846dae88ffc0193ca4b
David Fink <findaa21@gmail.com>
2025-05-05 13:03:17 +0200

    Workout und Exercice Routen funktionieren

--- backend/src/main/java/at/kaindorf/backend/repositorys/ExerciceRepository.java
+++ backend/src/main/java/at/kaindorf/backend/repositorys/ExerciceRepository.java
@@ -1,7 +1,15 @@
 package at.kaindorf.backend.repositorys;
 
 import at.kaindorf.backend.pojos.Exercice;
+import at.kaindorf.backend.pojos.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ExerciceRepository extends JpaRepository<Exercice,Integer> {
+
+    @Query("SELECT e FROM Exercice e")
+    Exercice getExercice();
+
+    @Query("SELECT e FROM Exercice e WHERE e.exerciseName = ?1")
+    Exercice getExerciseByExerciseNameName(String exerciseName);
 }
